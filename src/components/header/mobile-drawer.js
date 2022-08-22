07a04281eec7c44c88515d458e036105f092fe8b
Omar ♔ <o_yousaf@live.co.uk>
2022-08-22 22:57:47 +0100

    gop

--- src/components/header/mobile-drawer.js
+++ src/components/header/mobile-drawer.js
@@ -30,7 +30,7 @@ export default function MobileDrawer() {
             {menuItems.map((menuItem, i) => (
               <Link
                 activeClass="active"
-                path={menuItem.path}
+                to={menuItem.path}
                 spy={true}
                 smooth={true}
                 offset={-70}
