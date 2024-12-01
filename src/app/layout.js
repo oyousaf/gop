931edd41a4a4166a01dcae1a60f085872355f0f5
Omar ♔ <o_yousaf@live.co.uk>
2024-12-01 20:46:04 +0000

    squashing bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,47 @@
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = "G-ZYK38288LH";
+ReactGA.initialize(TRACKING_ID);
+
 export const metadata = {
   title: "حدائق الجنة",
   description: "Reviving the Ummah via sacred Islamic knowledge.",
+  author: "OYOUSAF,OYOUSAF87,OMAR YOUSAF,oyousaf_",
+  openGraph: {
+    title: "حدائق الجنة",
+    description: "Reviving the Ummah via sacred Islamic knowledge.",
+    type: "website",
+  },
+  twitter: {
+    card: "summary",
+    creator: "OYOUSAF,OYOUSAF87,OMAR YOUSAF,oyousaf_",
+    title: "حدائق الجنة",
+    description: "Reviving the Ummah via sacred Islamic knowledge.",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en-gb">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="description" content={metadata.description} />
+        <meta name="author" content={metadata.author} />
+        <meta name="title" content={metadata.title} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta
+          name="twitter:description"
+          content={metadata.twitter.description}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
