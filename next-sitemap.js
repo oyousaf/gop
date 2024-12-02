1b3d691dafec6ae0af14a01d857d511659ef3c73
Omar ♔ <o_yousaf@live.co.uk>
2024-12-02 16:49:13 +0000

    optimising SEO

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://oyousaf.uk',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'weekly',
+    priority: 0.7, 
+  };
+  
