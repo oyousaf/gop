ea131a1cc5863be021d624fbbc235eb456c155f1
Omar ♔ <o_yousaf@live.co.uk>
2024-12-02 17:03:46 +0000

    squashing bugs

--- src/app/components/Madinah.jsx
+++ src/app/components/Madinah.jsx
@@ -61,7 +61,7 @@ export default function Makkah() {
   }, []);
 
   return (
-    <section className="relative py-12 h-screen" id="makkah">
+    <section className="relative py-12 h-screen" id="madinah">
       <div className="max-w-5xl mx-auto flex justify-center items-center h-full z-10">
         <div className="text-center">
           <h2 className="md:text-4xl text-2xl font-bold mb-6 p-3">
