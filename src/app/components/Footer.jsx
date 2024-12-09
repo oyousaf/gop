738c191b0e23a19c95d35a2dd8fbe30e4433639a
Omar ♔ <o_yousaf@live.co.uk>
2024-12-09 16:52:44 +0000

    squashing bugs

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,10 +2,6 @@ import { useEffect, useState } from "react";
 import moment from "moment-hijri"; 
 import { socialLinks } from "../utils/constants";
 
-moment.locale("ar", {
-  numerals: true,
-});
-
 const getIslamicDate = () => {
   const islamicDate = moment().format('iD iMMMM iYYYY');
   return islamicDate;
