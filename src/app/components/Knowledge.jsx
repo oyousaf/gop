c23d1ddd2871ce91a1bfd151c0cab1adf8239147
Omar ♔ <o_yousaf@live.co.uk>
2024-12-18 13:46:08 +0000

    squashing bugs

--- src/app/components/Knowledge.jsx
+++ src/app/components/Knowledge.jsx
@@ -1,6 +1,6 @@
 export default function Knowledge() {
     return (
-      <div className="w-full h-72 flex justify-center items-center bg-gradient-to-r from-background to-teal-700 relative overflow-hidden shadow-lg">
+      <div className="w-full h-72 mb-[100] flex justify-center items-center bg-gradient-to-r from-background to-teal-700 relative overflow-hidden shadow-lg">
         <div className="text-center font-custom-font text-4xl md:text-6xl text-white leading-tight px-6 md:px-12">
           <span className="block mt-4">طلب العلم من المهد إلى اللحدا</span>
         </div>
