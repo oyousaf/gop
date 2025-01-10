5b48543130f2dd1e77b9639856e146420110fb37
Omar ♔ <o_yousaf@live.co.uk>
2025-01-10 17:24:10 +0000

    optimising SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,33 @@
 import GA from "./components/GA";
 
 export const metadata = {
-  title: "حدائق الجنة",
-  description: "Reviving the Ummah via sacred Islamic knowledge.",
-  author: "OYOUSAF,OYOUSAF87,OMAR YOUSAF,oyousaf_",
+  title: "حدائق الجنة - Sacred Islamic Knowledge",
+  description:
+    "Reviving the Ummah through sacred Islamic knowledge, Quranic teachings, and spiritual growth resources for a global Muslim community.",
+  author: "OYOUSAF, OYOUSAF87, OMAR YOUSAF, oyousaf_",
   keywords:
-    "Islam, Islamic knowledge, Quran, Hadith, Islamic teachings, Deen, sacred knowledge, Islamic faith, learn Islam, Islamic scholars, Islamic history, Quranic studies, Arabic language, Islamic teachings online, Islamic education, spiritual growth, Islamic community, worldwide Islamic knowledge, Islam for beginners, understanding Islam",
+    "Islamic knowledge, Quran, Hadith, Tafsir, Fiqh, Seerah, Deen, sacred knowledge, Islamic faith, learn Islam, Islamic teachings, Islamic scholars, Islamic history, Quranic studies, Arabic language, Islamic online courses, Islamic education, spiritual growth, Islamic spirituality, Islamic community, global Ummah, Islamic values, Islamic guidance, Islamic heritage, Islamic philosophy, Muslim lifestyle, Islamic parenting, Islamic beliefs, Islamic culture, Quran memorization, Islamic sermons, Islamic books, Islamic wisdom, Islamic resources, understanding Islam, Islamic lectures, Islam for beginners, Islamic jurisprudence, Islamic ethics, Islamic devotion, Islamic learning, Islam worldwide, Islamic practices, Islamic reminders, Islamic motivation, Sunnah, Zakah, Salah, Hajj, Umrah, Ramadan, Islamic unity, Islamic knowledge seekers, worldwide Islamic education, Quran and Sunnah, Islamic teachings online.",
   openGraph: {
-    title: "حدائق الجنة",
-    description: "Reviving the Ummah via sacred Islamic knowledge.",
+    title: "حدائق الجنة - Sacred Islamic Knowledge",
+    description:
+      "Discover the beauty of Islam with resources on Quranic teachings, Hadith, spiritual growth, and Islamic education.",
     type: "website",
   },
   twitter: {
     card: "summary",
-    creator: "OYOUSAF,OYOUSAF87,OMAR YOUSAF,oyousaf_",
-    title: "حدائق الجنة",
-    description: "Reviving the Ummah via sacred Islamic knowledge.",
+    creator: "OYOUSAF, OYOUSAF87, OMAR YOUSAF, oyousaf_",
+    title: "حدائق الجنة - Sacred Islamic Knowledge",
+    description:
+      "Reviving the Ummah with Quranic and Sunnah teachings for spiritual growth and learning.",
   },
   icons: {
-    icon: '/favicon.ico',
-  }
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en-gb">
-      <head>
-      </head>
       <body>
         <GA />
         {children}
