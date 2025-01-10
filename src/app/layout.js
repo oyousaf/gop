4d7b51e2d3791c1769837fae1d0116336907f678
Omar ♔ <o_yousaf@live.co.uk>
2025-01-10 17:38:55 +0000

    squashing bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import GA from "./components/GA";
 export const metadata = {
   title: "حدائق الجنة - Sacred Islamic Knowledge",
   description:
-    "Reviving the Ummah through sacred Islamic knowledge, Quranic teachings, and spiritual growth.",
+    "Reviving the Ummah through sacred Islamic knowledge.",
   author: "OYOUSAF, OYOUSAF87, OMAR YOUSAF, oyousaf_",
   keywords:
     "Islamic knowledge, Quran, Hadith, Tafsir, Fiqh, Seerah, Deen, sacred knowledge, Islamic faith, learn Islam, Islamic teachings, Islamic scholars, Islamic history, Quranic studies, Arabic language, Islamic online courses, Islamic education, spiritual growth, Islamic spirituality, Islamic community, global Ummah, Islamic values, Islamic guidance, Islamic heritage, Islamic philosophy, Muslim lifestyle, Islamic parenting, Islamic beliefs, Islamic culture, Quran memorization, Islamic sermons, Islamic books, Islamic wisdom, Islamic resources, understanding Islam, Islamic lectures, Islam for beginners, Islamic jurisprudence, Islamic ethics, Islamic devotion, Islamic learning, Islam worldwide, Islamic practices, Islamic reminders, Islamic motivation, Sunnah, Zakah, Salah, Hajj, Umrah, Ramadan, Islamic unity, Islamic knowledge seekers, worldwide Islamic education, Quran and Sunnah, Islamic teachings online.",
@@ -18,7 +18,7 @@ export const metadata = {
     creator: "OYOUSAF, OYOUSAF87, OMAR YOUSAF, oyousaf_",
     title: "حدائق الجنة - Sacred Islamic Knowledge",
     description:
-      "Reviving the Ummah with Quranic and Sunnah teachings for spiritual growth and learning.",
+      "Reviving the Ummah through sacred Islamic knowledge.",
   },
   icons: {
     icon: "/favicon.ico",
