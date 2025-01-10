b9e22563a2d06a9107011ed5dbd880d93346b7a9
Omar ♔ <o_yousaf@live.co.uk>
2025-01-10 17:39:15 +0000

    squashing bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
 import GA from "./components/GA";
 
 export const metadata = {
-  title: "حدائق الجنة - Sacred Islamic Knowledge",
+  title: "حدائق الجنة",
   description:
     "Reviving the Ummah through sacred Islamic knowledge.",
   author: "OYOUSAF, OYOUSAF87, OMAR YOUSAF, oyousaf_",
   keywords:
     "Islamic knowledge, Quran, Hadith, Tafsir, Fiqh, Seerah, Deen, sacred knowledge, Islamic faith, learn Islam, Islamic teachings, Islamic scholars, Islamic history, Quranic studies, Arabic language, Islamic online courses, Islamic education, spiritual growth, Islamic spirituality, Islamic community, global Ummah, Islamic values, Islamic guidance, Islamic heritage, Islamic philosophy, Muslim lifestyle, Islamic parenting, Islamic beliefs, Islamic culture, Quran memorization, Islamic sermons, Islamic books, Islamic wisdom, Islamic resources, understanding Islam, Islamic lectures, Islam for beginners, Islamic jurisprudence, Islamic ethics, Islamic devotion, Islamic learning, Islam worldwide, Islamic practices, Islamic reminders, Islamic motivation, Sunnah, Zakah, Salah, Hajj, Umrah, Ramadan, Islamic unity, Islamic knowledge seekers, worldwide Islamic education, Quran and Sunnah, Islamic teachings online.",
   openGraph: {
-    title: "حدائق الجنة - Sacred Islamic Knowledge",
+    title: "حدائق الجنة",
     description:
       "Discover the beauty of Islam with resources on Quranic teachings, Hadith, spiritual growth, and Islamic education.",
     type: "website",
@@ -16,7 +16,7 @@ export const metadata = {
   twitter: {
     card: "summary",
     creator: "OYOUSAF, OYOUSAF87, OMAR YOUSAF, oyousaf_",
-    title: "حدائق الجنة - Sacred Islamic Knowledge",
+    title: "حدائق الجنة",
     description:
       "Reviving the Ummah through sacred Islamic knowledge.",
   },
