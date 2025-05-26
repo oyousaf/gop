6727c3f567308dad59d3d4a0ec8365877914bd33
Omar 🍉 <o_yousaf@live.co.uk>
2025-05-26 15:39:36 +0100

    squashing bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -114,7 +114,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en-gb">
       <head />
-      <body className="bg-white text-gray-900 antialiased">
+      <body>
         <GA />
         {children}
       </body>
